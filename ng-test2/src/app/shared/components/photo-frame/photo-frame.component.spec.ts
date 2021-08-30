7d8e569046c9d7b7341a752f3a16bc49ca0206be
OtavioAugustto <otavioaugustto93@gmail.com>
2021-08-30 16:44:45 -0300

    implement new tests

--- ng-test2/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ ng-test2/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,7 +1,11 @@
 import { PhotoFrameModule } from './photo-frame.module';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { PhotoFrameComponent } from './photo-frame.component';
-
 
 describe(PhotoFrameComponent.name, () => {
   let fixture: ComponentFixture<PhotoFrameComponent> = null;
@@ -13,10 +17,68 @@ describe(PhotoFrameComponent.name, () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(PhotoFrameComponent);
-    component = fixture.componentInstance
+    component = fixture.componentInstance;
   });
 
-  it('Should create component', ()=>{
+  it('Should create component', () => {
     expect(component).toBeTruthy();
-  })
+  });
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+    Should trigger (@Output like) once when called multiple times within debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => times++);
+    component.like();
+    component.like();
+    tick(500);
+    expect(times).toBe(1);
+  }));
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+    Should trigger (@Output liked) two times when called outside debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => times++);
+    component.like();
+    tick(500);
+    component.like();
+    tick(500);
+    expect(times).toBe(2);
+  }));
+
+  it(`(D) Should display number of likes when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    expect(component.likes).toBe(1);
+    fixture.detectChanges();
+    const element: HTMLElement =
+      fixture.nativeElement.querySelector('.like-counter');
+    expect(element.textContent.trim()).toBe('1');
+  });
+
+  it(`(D) Should upadte aria-label when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('1: people liked');
+  });
+
+  it(`(D) Should have aria-label with 0 (@Input likes)`, () => {
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('0: people liked');
+  });
+
+  it(`(D) Should display image with src and description when bound to properties`, () => {
+    const description = 'some description';
+    const src = 'http://somesite.com/img.jpg';
+    component.src = src;
+    component.description = description;
+    fixture.detectChanges();
+    const img: HTMLImageElement = fixture.nativeElement.querySelector('img');
+    expect(img.getAttribute('src')).toBe(src);
+    expect(img.getAttribute('alt')).toBe(description);
+  });
 });
