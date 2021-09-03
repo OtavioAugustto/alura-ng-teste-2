76f25a6e4ec492f50e3cad1e8856f9d64f27e1c6
OtavioAugustto <otavioaugustto93@gmail.com>
2021-09-02 21:48:32 -0300

    Create new Dom Test

--- ng-test2/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ ng-test2/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -8,7 +8,7 @@ describe(LikeWidgetComponent.name, () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LikeWidgetModule]
+      imports: [LikeWidgetModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LikeWidgetComponent);
@@ -33,9 +33,24 @@ describe(LikeWidgetComponent.name, () => {
 
   it(`#${LikeWidgetComponent.prototype.like.name}
     should trigger (@Output liked) when called`, () => {
-      spyOn(component.liked, 'emit');
+    spyOn(component.liked, 'emit');
+    fixture.detectChanges();
+    component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
+  });
+
+  it(`(D) should display number of likes when clicked`, done => {
+    fixture.detectChanges();
+    component.liked.subscribe(() => {
+      component.likes++;
       fixture.detectChanges();
-      component.like();
-      expect(component.liked.emit).toHaveBeenCalled();
+      const counterEl: HTMLElement =
+        fixture.nativeElement.querySelector('.like-counter');
+      expect(counterEl.textContent.trim()).toBe('1');
+      done();
+    });
+    const likeWidgetContainerEl: HTMLElement =
+      fixture.nativeElement.querySelector('.like-widget-container');
+    likeWidgetContainerEl.click();
   });
 });
