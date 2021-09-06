8ced7b3273922f8b40f532889ff597e5ddb5f099
OtavioAugustto <otavioaugustto93@gmail.com>
2021-09-06 17:54:42 -0300

    Ajustments PhotoBoardComponent

--- ng-test2/src/app/shared/components/photo-board/photo-board.component.ts
+++ ng-test2/src/app/shared/components/photo-board/photo-board.component.ts
@@ -16,7 +16,7 @@ export class PhotoBoardComponent implements OnChanges {
     }
   }
 
-  public groupColumns(photos: Photo[]): any[][] {
+  private groupColumns(photos: Photo[]): any[][] {
     const newRows = [];
     const step = 4;
     for (let index = 0; index < photos.length; index += step) {
